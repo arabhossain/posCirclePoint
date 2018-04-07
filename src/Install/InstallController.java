fe485ecebfab79c2b14d78022ad7a355cda8ec8b
Arab Hossain <green.arab1995@gmail.com>
2018-04-07 12:35:19 +0600

    Configuration Classes

---
+++ src/Install/InstallController.java
@@ -0,0 +1,26 @@
+/*
+ * Developer: Arab Hossain
+ * Email: green.arab1995@gmail.com
+ */
+package Install;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author emonh
+ */
+public class InstallController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
